2c48cba9d4d0f79292cf32f070f9919f6a82031d
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-09-01 00:41:35 -0300

    feat: 🎸 Summary componente

---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,47 @@
+import styled, { css } from "styled-components";
+
+
+export const SummaryContainer = styled.section`
+    width: 100%;
+    max-width: 1120px;
+    margin: 0 auto;
+    padding: 0 1.5rem;
+
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 2rem;
+
+
+    margin-top: -5rem;
+`
+
+interface SummaryCardProps {
+    variant?: 'green'
+}
+
+export const SummaryCard = styled.div<SummaryCardProps>`
+
+    background-color: ${({ theme, variant }) => theme['gray-600']};
+    border-radius: 6px;
+    padding: 2rem;
+
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        color: ${({ theme }) => theme['gray-300']};
+    }
+
+
+    strong {
+        display: flex;
+        margin-top: 1rem;
+        font-size: 2rem;
+    }
+
+
+    ${({ variant }) => variant === 'green' && css`
+        background-color: ${({ theme }) => theme['green-700']};
+        `}
+
+`
